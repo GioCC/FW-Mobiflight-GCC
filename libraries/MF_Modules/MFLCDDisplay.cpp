b8fa4199260afef2dfd5f852acb8e24c7b8ed2d9
GiorgioCC <GCroci@ekinex.com>
2018-01-17 09:02:08 +0100

    First import - original repository, status 2017-09-05 https://bitbucket.org/mobiflight/mobiflightfc/overview

---
+++ libraries/MF_Modules/MFLCDDisplay.cpp
@@ -0,0 +1,70 @@
+// MFSegments.cpp
+//
+// Copyright (C) 2013-2014
+
+#include "MFLCDDisplay.h"
+
+MFLCDDisplay::MFLCDDisplay()
+{
+  _initialized = false;
+}
+
+void MFLCDDisplay::display(char *string)
+{
+  if (!_initialized) return;
+  char readBuffer[21] = "";
+  for(byte l=0;l!=_lines;l++) {
+    _lcdDisplay->setCursor(0, l);
+    memcpy(readBuffer, string+_cols*l, _cols);
+    _lcdDisplay->print(readBuffer);
+  }
+}
+
+void MFLCDDisplay::attach(byte address, byte cols, byte lines)
+{
+  _address = address;
+  _cols = cols;
+  _lines = lines;
+  _lcdDisplay = new LiquidCrystal_I2C( (uint8_t)address, (uint8_t)cols, (uint8_t)lines );
+  _initialized = true;
+  _lcdDisplay->begin();
+  _lcdDisplay->backlight();
+  test();
+}
+
+void MFLCDDisplay::detach()
+{
+  if (!_initialized) return;
+  delete _lcdDisplay;
+  _initialized = false;
+}
+
+void MFLCDDisplay::powerSavingMode(bool state)
+{
+  if (state)
+    _lcdDisplay->noBacklight();
+  else
+    _lcdDisplay->backlight();
+}
+
+void MFLCDDisplay::test() {
+  if (!_initialized) return;
+  
+  _lcdDisplay->setCursor(0, (_lines/2)-1);
+  for(byte c=0;c!=((_cols-10)/2);c++) {
+    _lcdDisplay->print(" ");
+  }
+  _lcdDisplay->print("Mobiflight");
+  for(byte c=0;c!=((_cols-10)/2);c++) {
+    _lcdDisplay->print(" ");
+  }
+  _lcdDisplay->setCursor(0, (_lines/2));
+  for(byte c=0;c!=((_cols-6)/2);c++) {
+    _lcdDisplay->print(" ");
+  }
+  _lcdDisplay->print("Rocks!");
+  for(byte c=0;c!=((_cols-6)/2);c++) {
+    _lcdDisplay->print(" ");
+  }
+  _lcdDisplay->setCursor(0, 0);
+}
